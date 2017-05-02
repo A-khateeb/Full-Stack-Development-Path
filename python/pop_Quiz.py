8ce843144c044a5e7b39ea85285b5fcdc34f2236
afeefkhateeb <khateebafeef@gmail.com>
2017-05-02 22:47:46 +0300

    Commit lesson3 Udacity

---
+++ python/pop_Quiz.py
@@ -0,0 +1,24 @@
+p = [1,2]
+p.append(3)
+p.pop()
+print (p)
+
+c = [1,2]
+x = c.pop()
+y= c.pop()
+c.append(x)
+c.append(y)
+print (c)
+
+
+t = [1,2]
+g = t.pop()
+t.append(g)
+print (t)
+
+u = [1,2]
+i= u.pop()
+o= u.pop()
+u.append(o)
+u.append(i)
+print (u)
