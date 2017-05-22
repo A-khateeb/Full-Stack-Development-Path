8ba79cd8f73f047184d04047e1d718a7d9e1e048
afeefkhateeb <khateebafeef@gmail.com>
2017-05-22 22:32:19 +0300

    Add Code related to the number of clicks on Web crawler.

--- python/number_of_clicks.py
+++ python/number_of_clicks.py
@@ -1,24 +1,19 @@
-# The current index includes a url in the list of urls
-# for a keyword multiple times if the keyword appears
-# on that page more than once.
-
-# It might be better to only include the same url
-# once in the url list for a keyword, even if it appears
-# many times.
-
-# Modify add_to_index so that a given url is only
-# included once in the url list for a keyword,
-# no matter how many times that keyword appears.
+def record_user_click(index, keyword, url):
+    urls = lookup(index, keyword)
+    if urls:
+        for entry in urls:
+            if entry[0] == url:
+                entry[1] = entry[1]+1
 
 def add_to_index(index, keyword, url):
     for entry in index:
         if entry[0] == keyword:
-            if url not in entry[1]:
-                entry[1].append(url)
+            for urls in entry[1]:
+                if urls[0] == url:
+                    return
+            entry[1].append([url,0])
             return
-    # not found, add new keyword to index
-    index.append([keyword, [url]])
-
+    index.append([keyword, [[url,0]]])
 
 def get_page(url):
     try:
@@ -28,8 +23,7 @@ def get_page(url):
 <a href="http://www.udacity.com/cs101x/crawling.html">
 learn to crawl</a> before you try to
 <a href="http://www.udacity.com/cs101x/walking.html">walk</a> or
-<a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body>
-</html>'''
+<a href="http://www.udacity.com/cs101x/flying.html">fly</a>.</p></body></html>'''
 
         elif url == "http://www.udacity.com/cs101x/crawling.html":
             return '''<html> <body> I have not learned to crawl yet, but I am
@@ -38,11 +32,10 @@ quite good at  <a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.
 
         elif url == "http://www.udacity.com/cs101x/walking.html":
             return '''<html> <body> I cant get enough
-<a href="http://www.udacity.com/cs101x/index.html">crawling</a></body></html>'''
+<a href="http://www.udacity.com/cs101x/index.html">crawling</a>!</body></html>'''
 
         elif url == "http://www.udacity.com/cs101x/flying.html":
-            return '''<html>
-<body>The magic words are Squeamish Ossifrage!</body></html>'''
+            return '<html><body>The magic words are Squeamish Ossifrage!</body></html>'
     except:
         return ""
     return ""
@@ -96,6 +89,13 @@ def lookup(index, keyword):
             return entry[1]
     return None
 
-#index = crawl_web("http://www.udacity.com/cs101x/index.html")
-#print lookup(index,"is")
-#>>> ['http://www.udacity.com/cs101x/index.html']
+
+#Here is an example showing a sequence of interactions:
+index = crawl_web('http://www.udacity.com/cs101x/index.html')
+print (lookup(index, 'good'))
+#>>> [['http://www.udacity.com/cs101x/index.html', 0],
+#>>> ['http://www.udacity.com/cs101x/crawling.html', 0]]
+record_user_click(index, 'good', 'http://www.udacity.com/cs101x/crawling.html')
+print (lookup(index, 'good'))
+#>>> [['http://www.udacity.com/cs101x/index.html', 0],
+#>>> ['http://www.udacity.com/cs101x/crawling.html', 1]]
