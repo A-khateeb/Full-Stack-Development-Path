55131fe2a9fba5a6225616dfe261195697819a9e
A-khateeb <khateebafeef@gmail.com>
2018-01-01 21:46:28 +0200

    Push Print Properties

---
+++ creativity/C1.26.py
@@ -0,0 +1,6 @@
+s = input("Insert Three numbers separated by Blank\n").split())
+print(s)
+a,b,c=(int(i) for i in s)
+print("Is {}+{}={}".format(a,b,c),a+b==c)
+print("Is {}={}-{}".format(a,b,c),a==b-c)
+print("Is {}*{}={}".format(a,b,c),a*b==c)
