8094559917c2392e3fadd4bd02228c32c2f122b5
A-khateeb <khateebafeef@gmail.com>
2018-03-18 23:29:25 +0200

    Commit changes to the flow

--- Object_oriented/CreditCard.py
+++ Object_oriented/CreditCard.py
@@ -1,9 +1,10 @@
 class CreditCard:
-    def __init__(self, customer, bank, account, limit):
+    def __init__(self, customer, bank, account, limit, status):
         self._customer = customer
         self._bank= bank
         self._account= account
         self._limit= limit
+        self._status= status
         self._balance = 0
     def get_customer(self):
         return self._customer
@@ -15,7 +16,8 @@ class CreditCard:
         return self._limit
     def get_balance(self):
         return self._balance
-
+    def get_status(self):
+        return self._status
     def charge(self, price):
         if price+ self._balance > self._limit:
             return False
@@ -26,26 +28,30 @@ class CreditCard:
     def make_payment(self, amount):
         self._balance=amount
 
-cc= CreditCard('Afeef', '1st bank', '12345678910', 1000)
+cc= CreditCard('Afeef', '1st bank', '12345678910', 1000, "status")
 
 
 if __name__ == '__main__':
     wallet = []
-    wallet.append(CreditCard('John Adams','Arab_Bank','123456789',200 ))
-    wallet.append(CreditCard('Afeef Khateeb','Quds_bank','7894561230',200000 ))
-    wallet.append(CreditCard('Moh Ahamd','TNB','45612358',7800 ))
-    wallet.append(CreditCard('Ahmad Ahmad','Palestine_Bank','456123789',200 ))
+    print("What is your password")
+    print("Keep it Secret")
+    passwd = input()
+    wallet.append(CreditCard('John Adams','Arab_Bank','123456789',200,"Open" ))
+    wallet.append(CreditCard('Afeef Khateeb','Quds_bank','7894561230',200000 ,"Closed" ))
+    wallet.append(CreditCard('Moh Ahamd','TNB','45612358',7800,"Closed"))
+    wallet.append(CreditCard('Ahmad Ahmad','Palestine_Bank','456123789',200 ,"Open"))
     for val in range(1,17):
         wallet[0].charge(val)
         wallet[1].charge(val*2)
         wallet[2].charge(val*3)
-    for c in range(3):
-        print("Customer:", wallet[c].get_customer())
-        print("Bank:", wallet[c].get_bank())
-        print("Account Number:", wallet[c].get_account())
-        print("Account Limit:" , wallet[c].get_limit())
-        print("Account Balance:" , wallet[c].get_balance())
-        while wallet[c].get_balance() > 100:
-            wallet[c].make_payment(100)
-            print("New Balance", wallet[c].get_balance())
-            print()
+        for c in range(3):
+            print("Customer:", wallet[c].get_customer())
+            print("Bank:", wallet[c].get_bank())
+            print("Account Number:", wallet[c].get_account())
+            print("Account Limit:" , wallet[c].get_limit())
+            print("Account Balance:" , wallet[c].get_balance())
+            print("Account Status:" , wallet[c].get_status())
+            while wallet[c].get_balance() > 100:
+                wallet[c].make_payment(100)
+                print("New Balance", wallet[c].get_balance())
+                print()
