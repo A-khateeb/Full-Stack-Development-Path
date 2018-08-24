5178773280d7d9efcd42b43ea457e1825c71c22c
A-khateeb <khateebafeef@gmail.com>
2018-08-24 18:18:37 +0300

    Variables

---
+++ python3/LPTHW4.py
@@ -0,0 +1,16 @@
+my_name = 'Afeef Khateeb'
+my_age = 35
+my_height = 74
+my_weight = 184
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Lets talk about my name {my_name}")
+print(f"He is {my_height} inches tall")
+print(f"He's {my_weight} pound heavy ")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} depending on the coffee")
+total =my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_weight},and {my_height} I will get {total}")
+
