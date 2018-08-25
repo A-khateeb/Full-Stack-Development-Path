7925877900f1a30ee2f4943c03d2d2963fd953a0
A-khateeb <khateebafeef@gmail.com>
2018-08-25 23:45:24 +0300

    COmmiting games

---
+++ python3/LPTHW14.py
@@ -0,0 +1,17 @@
+from sys import argv
+script, user_name = argv
+prompt = '>'
+print(f"Hi {user_name} I am {script} script")
+print("I'd like to ask you few questions")
+print(f"Do you like me {user_name}")
+likes = input(prompt)
+print(f"Where do you live {user_name}")
+lives = input(prompt)
+print(f"What kind of computer do you have {user_name}")
+computer = input(prompt)
+
+print(f"""
+Alright, so you said {likes} about liking me
+You live in {lives}. Not sure where that is
+And you have a computer {computer}. Nice
+""")
