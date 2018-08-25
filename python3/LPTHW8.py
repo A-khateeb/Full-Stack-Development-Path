9074eab7dc3a2e66673cb6688718a5de4422ec08
A-khateeb <khateebafeef@gmail.com>
2018-08-25 14:01:35 +0300

    Escape chars

---
+++ python3/LPTHW8.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days:",days)
+print("Here are the months:",months)
+print("""
+There is somethong going on there
+With three quotes
+We will be able to type as much as we like
+Even 4 lines if we want, or 5 or 6
+""")
---
+++ python3/LPTHW9.py
@@ -0,0 +1,30 @@
+print("I am 6'2\" tall")
+print('I am 6\'2" tall')
+################################
+tobby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat  = '''
+I'll do a list:
+\t*cat food
+\t*Fishies
+\t*Catnip\n\t*Grass
+\f formfeed
+\b \b Backspace
+\a formfeed
+'''
+tiny_cat = '''
+\n hello \n
+\n hello /n
+World
+\t tabbed
+\\ Hello \\
+//Hello//
+'''
+
+print(tobby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(tiny_cat)
