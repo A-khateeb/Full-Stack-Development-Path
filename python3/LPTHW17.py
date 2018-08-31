e4a221e1f8f3f384a7ffc10eba59bacaea65b640
A-khateeb <khateebafeef@gmail.com>
2018-08-31 19:14:46 +0300

    Adding new file append

---
+++ python3/LPTHW17.py
@@ -0,0 +1,18 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}")
+in_file = open(from_file)
+indata = in_file.read()
+
+print(f"The input file is {len(indata)} bytes long")
+print("Ready, hit enter to continue, CTRL-C to abort")
+
+input()
+out_file = open(to_file, "w")
+out_file.write(indata)
+print("Alright, all done")
+out_file.close()
+in_file.close()
