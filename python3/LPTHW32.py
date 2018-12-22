d2ae85cb54d3605eb7322e4e0c0eef7fbcf038c4
A-khateeb <khateebafeef@gmail.com>
2018-12-22 22:59:00 +0200

    Add to Head REPO

--- python3/LPTHW32.py
+++ python3/LPTHW32.py
@@ -1,12 +1,25 @@
-the_count = [1,2,3,5,5]
+the_count = [1,2,3,4,5]
 fruits = ["Apple","Orange","Pears","Apricots"]
 Change = [1,"Pennis",2,"Dims",3,"Quarter"]
 
 for number in the_count:
-    print(f"The number is {number}")
+    print(f"This is count {number}")
 
 for fruit in fruits:
-    print(f"The fruits are {fruit}")
+    print(f"The fruits type: {fruit}")
 
 for change in Change:
-    print(f"The change I have is {change}")
+    print(f"I got {change}")
+
+element = []
+
+for i in range(0,6):
+    if i == 0 or i == 1:
+        print(f"Adding {i} element to the list.")
+        element.append(i)
+    else:
+        print(f"Add {i} elements to the list.")
+        element.append(i)
+
+for i in element:
+    print(f"The elements are: {i}")
