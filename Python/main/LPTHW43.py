8db75e01920bdf2b9eec9f852de058783205b372
A-khateeb <khateebafeef@gmail.com>
2019-03-02 22:15:47 +0200

    Add to main HEAD

---
+++ Python/main/LPTHW43.py
@@ -0,0 +1,8 @@
+'''
+Death: This is when the player dies
+Central Corridor: The starting point and where the player should defeat the Gothons (Aliens)
+Lazer weapon Armory: A neutron bomb to blow the ship up before escaping the pod.
+-->It has a keypad that the hero needs to guess the number
+The bridge: Another battle scene where the hero places the bomb
+Escape pod: Where the hero escapes but only after guessing the number 
+'''
