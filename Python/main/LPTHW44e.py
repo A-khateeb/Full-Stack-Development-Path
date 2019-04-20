29b99b1bea42a57fc6cd65efe557e298ca8fc523
A-khateeb <khateebafeef@gmail.com>
2019-04-20 18:36:58 +0300

    Add to REPO MASTER

---
+++ Python/main/LPTHW44e.py
@@ -0,0 +1,28 @@
+class Other(object):
+    def override(self):
+        print("OTHER override()")
+
+    def implicit(self):
+        print("OTHER implicit()")
+
+    def altered(self):
+        print("OTHER altered()")
+
+class Child(object):
+    def __init__(self):
+        self.other = Other()
+    def implicit(self):
+        self.other.implicit()
+    def override(self):
+#        self.other.override()
+        print("Child Override")
+    def altered(self):
+        print("CHILD Before altered()")
+        self.other.altered()
+        print("Child after altered()")
+
+son = Child()
+
+son.implicit()
+son.altered()
+son.override()
