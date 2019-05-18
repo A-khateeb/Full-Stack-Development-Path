dc2d9bd0df4542e7256b3c2205a483d08a4951ee
A-khateeb <khateebafeef@gmail.com>
2019-05-18 22:30:04 +0300

    Adding new PCC files

---
+++ Python/PCC/hello_world.py
@@ -0,0 +1,6 @@
+messages = "Hello Python World!"
+print(messages)
+messages = "Hello Python Crash Course World!"
+print(messages)
+
+ 
---
+++ Python/PCC/simple_messages.py
@@ -0,0 +1,4 @@
+messages = "Hello World of programmers"
+print(messages)
+messages = "Value of the variable is changed"
+print(messages)
