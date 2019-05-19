2f3d431f564fc2afdcc4e38b632fa0b7110e2bc6
A-khateeb <khateebafeef@gmail.com>
2019-05-19 23:50:43 +0300

    Adding chapter 2

---
+++ Python/PCC/nameCases2.4.py
@@ -0,0 +1,4 @@
+name = "afeef"
+print("Lower case " + " ' " + name.lower() + " '")
+print("Upper case " + " ' " + name.upper() + " '")
+print("Title " + " ' " + name.title() + " '")
---
+++ Python/PCC/strippingNames2.7.py
@@ -0,0 +1,5 @@
+name = " \tAfeef\n "
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
