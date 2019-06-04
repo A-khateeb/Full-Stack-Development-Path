5d581328bb2c20f4f47f82adc003e2668317f2af
A-khateeb <khateebafeef@gmail.com>
2019-06-04 18:50:14 +0300

    Adding IF statment to REPO

---
+++ Python/PCC/Chapter5/If_Statement.py
@@ -0,0 +1,11 @@
+cars = ["audi","bmw","subaru","toyota"]
+for i in cars:
+    if i == "audi":
+        print(i.upper())
+    else:
+        print(i.title())
+
+#set the variable i to bmw
+i = 'bmw'
+#check the equality of the variable (equality operator)
+i == 'bmw'
