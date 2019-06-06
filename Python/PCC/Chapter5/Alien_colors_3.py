b838f58206b58cee8a2705727ce6fa0b4011fbad
Afeef Khateeb <khateebafeef@gmail.com>
2019-06-06 12:46:44 +0000

    Adding to REPO MASTER from Ubuntu

--- Python/PCC/Chapter5/Alien_colors_3.py
+++ Python/PCC/Chapter5/Alien_colors_3.py
@@ -4,12 +4,39 @@ color = "green"
 if color == "green":
     print("You just earned 5 points")
 
-if color != "green":
+elif color != "yellow":
     print("You just earned 10 points")
 
 
-if color == "blue":
+elif color == "red":
     print("You just earned 5 points")
 
 else:
-    print("You just earned 10 points")
+    print("You did not earn anything")
+ 
+color = "yellow"
+if color == "green":
+     print("You just earned 5 points")
+ 
+elif color != "yellow":
+     print("You just earned 10 points")
+ 
+elif color == "red":
+     print("You just earned 5 points")
+
+else:
+     print("You did not earn anything")
+ 
+color = "red"
+if color == "green":
+     print("You just earned 5 points")
+ 
+elif color != "yellow":
+     print("You just earned 10 points")
+ 
+ 
+elif color == "red":
+     print("You just earned 5 points")
+ 
+else:
+    print("You did not earn anything")
---
+++ Python/PCC/Chapter5/Favorite_fruit.py
@@ -0,0 +1,11 @@
+fruit = ["banana", "melon", "apple","orange"]
+if 'banana' in fruit:
+    print("You really like bananas")
+if "melon" in fruit:
+    print("You really like melons")
+if 'apple' in fruit:
+    print("You really like apples")
+if 'orange' in fruit:
+    print("You really like oranges")
+if "Kiwi" in fruit:
+    print("You really like Kiwi")
