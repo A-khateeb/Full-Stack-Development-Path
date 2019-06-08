80a82a36ab5ef351f3b50d9878123db0379f92d0
A-khateeb <khateebafeef@gmail.com>
2019-06-08 23:49:57 +0300

    Adding new Code

---
+++ Python/PCC/Chapter6/FavoriteNumbers.py
@@ -0,0 +1,9 @@
+person = {"Afeef":[1,9],
+          "Ahmad":[2,10],
+          "Samir":[7,14],
+          "Mustafa":[10,12]}
+
+for i,l in person.items():
+    print(i)
+    for r in l:
+        print(r)
