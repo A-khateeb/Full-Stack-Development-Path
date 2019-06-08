b024bd1be0c6773889d98a18a220dcf2430fb09c
A-khateeb <khateebafeef@gmail.com>
2019-06-08 23:44:07 +0300

    Adding to Repo MASTER

---
+++ Python/PCC/Chapter6/FavoritePlaces.py
@@ -0,0 +1,9 @@
+names = {
+'Ahmad':['Jerusalem','Beit Lahem'],
+'Afeef':['Amman','Istanbul'],
+'Kamal':['Cairo','Muzambuq'],
+}
+for i,m in names.items():
+    print(i + " Favorite places are:")
+    for l in m:
+        print("\t"+l)
