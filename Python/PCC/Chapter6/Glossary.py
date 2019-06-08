10eea8e71c5690a021623726ecb51f1ca55081c4
A-khateeb <khateebafeef@gmail.com>
2019-06-08 15:11:35 +0300

    Adding Glossary Dict

---
+++ Python/PCC/Chapter6/Glossary.py
@@ -0,0 +1,20 @@
+glossary = {"Dictionary":"Is a key-value pair structure",
+            "del":"is a method used to delete the list or Dictionary without having any way to retrive data",
+            "Lists":"Is an array based structure",
+            "Tuple":"Is a list, however, the values cannot change",
+            "pop":"is a method used to delete the list or Dictionary without having any way to retrive data"}
+
+print("Dictionary in python:")
+print(glossary["Dictionary"]+"\n")
+
+print("del in python:")
+print(glossary["del"]+"\n")
+
+print("Lists in python:")
+print(glossary["Lists"]+"\n")
+
+print("Tuple in python:")
+print(glossary["Tuple"]+"\n")
+
+print("pop in python:")
+print(glossary["pop"]+"\n")
