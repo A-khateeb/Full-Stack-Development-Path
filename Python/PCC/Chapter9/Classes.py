da458da3f893d19fe90f591a5960b1c5a023bcbd
A-khateeb <khateebafeef@gmail.com>
2019-06-30 23:39:46 +0300

    Adding to repo HEAD

---
+++ Python/PCC/Chapter9/Classes.py
@@ -0,0 +1,9 @@
+class Dog():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(self.name.title() + " is sitting now")
+    def roll_over(self):
+        print(self.name.title() + " rolled over!")
