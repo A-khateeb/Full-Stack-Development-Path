56e2ceece122a33293bf5d07294cee3085b13d87
A-khateeb <khateebafeef@gmail.com>
2019-07-02 23:49:51 +0300

    adding t

--- Python/PCC/Chapter9/Classes.py
+++ Python/PCC/Chapter9/Classes.py
@@ -7,3 +7,8 @@ class Dog():
         print(self.name.title() + " is sitting now")
     def roll_over(self):
         print(self.name.title() + " rolled over!")
+
+my_dog = Dog('willie' , 6)
+print("My dogs name is " + my_dog.name.title())
+print("My dogs age is " + str(my_dog.age))
+my_dog.name
