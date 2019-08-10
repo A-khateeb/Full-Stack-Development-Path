bb7ebcefe62db5fb51cc99eb8e7ae9a6233f2076
A-khateeb <khateebafeef@gmail.com>
2019-08-10 22:35:21 +0300

    Adding new resources

--- Python/PCC/Chapter9/Classes.py
+++ Python/PCC/Chapter9/Classes.py
@@ -1,14 +1,24 @@
 class Dog():
     def __init__(self, name, age):
-        self.name = name
-        self.age = age
+#        pass
+         self.name = name
+         self.age = age
 
     def sit(self):
-        print(self.name.title() + " is sitting now")
-    def roll_over(self):
-        print(self.name.title() + " rolled over!")
+        print(self.name.title()+" is now sitting")
 
-my_dog = Dog('willie' , 6)
-print("My dogs name is " + my_dog.name.title())
-print("My dogs age is " + str(my_dog.age))
-my_dog.name
+    def roll_over(self):
+        print(self.name.title() + " is rolling over")
+
+
+my_dog = Dog('willie', 26)
+your_dog = Dog('lucy', 5)
+print("My dog's name is " + my_dog.name.title())
+print("My dog's age is " + str(my_dog.age) + " years old")
+my_dog.roll_over()
+my_dog.sit()
+print("\n\n")
+print("Your dog's name is " + your_dog.name.title())
+print("Your dog's age is " + str(your_dog.age) + " years old")
+your_dog.roll_over()
+your_dog.sit()
