fdc05dfea28a19372c73dcb116da9434e6e73b20
Afeef <khateebafeef@gmail.com>
2019-08-14 00:11:51 +0300

    Adding to REPO Master

--- Python/PCC/Chapter9/LoginAttempts.py
+++ Python/PCC/Chapter9/LoginAttempts.py
@@ -1,16 +1,37 @@
 class Users():
-	def __init__(self, Fname, Lname, DOB):
-		self.Fname = Fname
-		self.Lname = Lname
-		self.DOB = DOB
-	def dateBirth(self):
-		return(self.Fname + " date of birth is " + self.DOB)
+     def __init__(self, Fname, Lname, DOB):
+         self.Fname = Fname
+         self.Lname = Lname
+         self.DOB = DOB
+         self.loginAt = 0
 
-	def user_desc(self):
-		return("Welcome " + self.Fname + " to our website. Please confirm on the below data" + '\n' + "First Name: " + self.Fname + "\n" + "Last Name: " +self.Lname + "\n" + "Date of Birth: "+self.DOB)
+     def dateBirth(self):
+         return(self.Fname + " date of birth is " + self.DOB)
+
+     def user_desc(self):
+         return("Welcome " + self.Fname + " to our website. Please confirm on the below data" + '\n' + "First Name: " + self.Fname + "\n" + "Last Name: " +self.Lname + "\n" + "Date of Birth: "+self.DOB)
+
+     def increment_login(self):
+         self.loginAt += 1
+         login = "Login attempts " + str(self.loginAt)
+         print(login)
+
+     def reset_login(self):
+         self.loginAt = 0
+
 
 
 
 my_user = Users("Afeef" , "Khateeb", "30/7/1991")
 print(my_user.dateBirth())
 print(my_user.user_desc())
+my_user.increment_login()
+my_user.increment_login()
+my_user.increment_login()
+my_user.increment_login()
+my_user.increment_login()
+my_user.increment_login()
+my_user.reset_login()
+my_user.increment_login()
+my_user.increment_login()
+my_user.increment_login()
