4b774af5299a6109b5fe499b0533c3d24d9fc747
Afeef <khateebafeef@gmail.com>
2019-08-14 23:14:18 +0300

    Add new code to repo Head

--- Python/PCC/Chapter9/Classes.py
+++ Python/PCC/Chapter9/Classes.py
@@ -1,28 +1,28 @@
-# class Dog():
-#     def __init__(self, name, age):
-# #        pass
-#          self.name = name
-#          self.age = age
-#
-#     def sit(self):
-#         print(self.name.title()+" is now sitting")
-#
-#     def roll_over(self):
-#         print(self.name.title() + " is rolling over")
-#
-#
-# my_dog = Dog('willie', 26)
-# your_dog = Dog('lucy', 5)
-# print("My dog's name is " + my_dog.name.title())
-# print("My dog's age is " + str(my_dog.age) + " years old")
-# my_dog.roll_over()
-# my_dog.sit()
-# print("\n\n")
-# print("Your dog's name is " + your_dog.name.title())
-# print("Your dog's age is " + str(your_dog.age) + " years old")
-# your_dog.roll_over()
-# your_dog.sit()
-#
+class Dog():
+    def __init__(self, name, age):
+#        pass
+         self.name = name
+         self.age = age
+
+    def sit(self):
+        print(self.name.title()+" is now sitting")
+
+    def roll_over(self):
+        print(self.name.title() + " is rolling over")
+
+
+my_dog = Dog('willie', 26)
+your_dog = Dog('lucy', 5)
+print("My dog's name is " + my_dog.name.title())
+print("My dog's age is " + str(my_dog.age) + " years old")
+my_dog.roll_over()
+my_dog.sit()
+print("\n\n")
+print("Your dog's name is " + your_dog.name.title())
+print("Your dog's age is " + str(your_dog.age) + " years old")
+your_dog.roll_over()
+your_dog.sit()
+
 class Car():
     def __init__(self, make, model, year):
         self.make = make
@@ -42,9 +42,43 @@ class Car():
             self.odometer_reading = mileage
         else:
             print("You cannot roll back the odometer!")
+
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
+    def gas_tank_fill(self):
+        return("This car needs to be filled in gas ")
+
+class Battery():
+    def __init__(self, battery_size = 70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        return("This cas has a "+ str(self.battery_size) + "-kmh battery")
+
+    def get_range(self):
+        if self.battery_size == 70 :
+            range = 240
+        elif self.battery_size == 85:
+            range = 285
+        message = "This car can go approximately " + str(range)
+        message += ' miles on full charge '
+        return (message)
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def gas_tank_fill(self):
+        return("This cas does not need to be filled in fuel!")
+
+my_tesla = ElectricCar('Tesla', 'model s ', 2016)
+print(my_tesla.get_descriptive_name())
+print(my_tesla.battery.describe_battery())
+print(my_tesla.battery.get_range())
+print(my_tesla.gas_tank_fill())
 new_car = Car('Audi' , 'A4', 2019)
 print(new_car.get_descriptive_name())
 new_car.update_odometer(23500)
---
+++ Python/PCC/Chapter9/IceCreamStand.py
@@ -0,0 +1,43 @@
+class Restaurant():
+     def __init__(self, rname, rType):
+         self.rname = rname
+         self.rType = rType
+         self.number_served = 10
+
+     def open_restauarant(self):
+         return(self.rname + " is open now")
+
+     def customers_served(self):
+         return("The number of customers served is " + str(self.number_served))
+     def describe_restaurant(self):
+         return(self.rname + " is a " +self.rType)
+
+     def set_number_served(self, customers):
+         self.number_served = customers
+     def increment_served(self, customers):
+         self.number_served += customers
+
+
+
+class IceCream(Restaurant):
+    def __init__(self, rname, rType):
+        super().__init__(rname, rType)
+        self.rname = rname
+        self.rType = rType
+    def describe_flavors(self):
+        flavoree = ["A","B","C"]
+        for item in flavoree:
+            pass
+        message = "The flavor you are searching for is "  + item
+        return(message)
+
+new_rest = Restaurant("Pizza Hut" , "French cuzine")
+print(new_rest.rname.title())
+print(new_rest.describe_restaurant())
+ice = IceCream("Pizza Hut" , "French cuzine")
+print(ice.describe_flavors())
+print()
+#new_rest.number_served = 66
+new_rest.set_number_served(50)
+new_rest.increment_served(10)
+print(new_rest.customers_served())
