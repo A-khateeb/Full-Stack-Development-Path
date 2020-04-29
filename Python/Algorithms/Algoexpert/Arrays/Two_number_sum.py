afeb5c012b6f27548492516b57111ff3d72ddb79
A-khateeb <khateebafeef@gmail.com>
2020-04-29 16:41:03 +0300

    Adding new material

---
+++ Python/Algorithms/Algoexpert/Arrays/Two_number_sum.py
@@ -0,0 +1,12 @@
+"""
+Build a function that has an input as follows and
+Inpurt array of distinct numbers and and target sum.The array must be checked for two pairs summed together to give the result of the target and the result must be a the target number
+
+Example
+INPUT
+array = [3,5,-4,8,11,1,-1,6]
+target = 10
+
+OUTPUT
+[-1,11]
+"""
